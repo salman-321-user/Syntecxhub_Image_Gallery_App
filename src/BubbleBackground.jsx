9713fba9a49a280f77b089545eabb666a2425fc0
Salman Haider <shadowgameof850@gmail.com>
2026-02-19 13:15:05 +0500

    Initial commit - Image Gallery App with React + Tailwind

---
+++ src/BubbleBackground.jsx
@@ -0,0 +1,21 @@
+// BubbleBackground.jsx
+import React from 'react';
+import './index.css';
+
+const BubbleBackground = () => {
+  return (
+    <div className="bubble-background">
+      <div className="bubbles">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BubbleBackground;
