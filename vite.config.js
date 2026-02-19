918890684baf88eb2801ddcef3d737cb698178b8
Salman Haider <shadowgameof850@gmail.com>
2026-02-19 14:55:51 +0500

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: true
   },
-  base: '/Syntecxhub_Image_Gallery_App/',
+  base: '/Syntecxhub_Image_Gallery_App/', // Make sure this matches your repo name exactly
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
